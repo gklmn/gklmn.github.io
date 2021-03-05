7fe803460f655baa1796bf1a5a284a0ddbae320e
gklmn <16239136+gklmn@users.noreply.github.com>
2021-03-05 16:22:46 +0100

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,20 @@
 
 // Repère le warning "pas de tarif" et cache le parent
-var myDiv = Array.prototype.slice.call(document.querySelectorAll('notification'));
+var noFareRows = Array.prototype.slice.call(document.querySelectorAll('notification'));
 
-myDiv.map(function (x) {
+noFareRows.map(function (x) {
     if (x.parentNode.tagName === 'PRICE-LAYOUT') {
         x.parentNode.style.color = 'red';
         x.parentNode.style.display = 'none';
         x.parentNode.classList.add("test-hidden");
     }
-    x.style.color = "red";
-    x.style.display = "none";
-    x.classList.add("test-hidden");
+    //x.style.color = "red";
+    // x.style.display = "none";
+    //x.classList.add("test-hidden");
 
 });
+
+
+
+
+
